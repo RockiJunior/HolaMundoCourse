4ea6547c25cdf4a17bd8821528704fb2795cf985
Gabriel Villarroel <78510297+RockiJunior@users.noreply.github.com>
2022-01-13 13:01:02 -0300

    fetching

--- 03-arrayMethods.js
+++ 03-arrayMethods.js
@@ -20,11 +20,11 @@ const mapped2 = arr.map((el) => `<h1>${el}</h1>`);
 //=> esto devuelve un array con elementos html y los elementos dentro de los mismos
 
 const users = [
-    {id: 1, name: "John1"},
-    {id: 2, name: "John2"},
-    {id: 3, name: "John3"},
-    {id: 4, name: "John4"},
-]
+  { id: 1, name: "John1" },
+  { id: 2, name: "John2" },
+  { id: 3, name: "John3" },
+  { id: 4, name: "John4" },
+];
 const usersMapped = users.map((user) => `<h1>${user.name}</h1>`);
 // console.log(usersMapped)
 
@@ -33,7 +33,7 @@ const arrReduced = arr.reduce((acc, el) => acc + el, 0);
 // el acc, es el valor de cada elemento del array, por cada pasada del loop
 // el, es el elemento actual del loop
 // 0 es el valor inicial del acumulador
-// Ejemplo por vueltas: 
+// Ejemplo por vueltas:
 // 0 + 0 = 0
 // el acc es 0 + el elemento 0 del array que es 0
 // 0 + 1 = 1
@@ -50,5 +50,23 @@ const arrReduced = arr.reduce((acc, el) => acc + el, 0);
 const getMaxValue = (a, b) => Math.max(a, b);
 
 const arrGetMaxReduced = arr.reduce(getMaxValue, 0);
-console.log(arrGetMaxReduced)
+//si en el array contenemos algun tipo de valor negativo, sencillamente eliminamos el cero de la derecha
+//para que el valor inicial sea el negativo maximo:
 
+// const arrGetMaxReduced = arr.reduce(getMaxValue);
+// console.log(arrGetMaxReduced)
+
+// const reducedUsers = users.reduce(
+//   (acc, el) => `${acc === " " ? " " : `${acc},`}${el.name}`,
+//   " "
+// );
+
+const reducedUsers = users.reduce((acc, el) => {
+  if (el.id < 2) {
+    return acc;
+  } else {
+    return acc.concat(el)
+  }
+}, []);
+
+console.log(reducedUsers);
