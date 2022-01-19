44e9d68a6cc534180b238b6913b00721675e6e5e
Gabriel Villarroel <78510297+RockiJunior@users.noreply.github.com>
2022-01-19 11:05:27 -0300

    19-01 files modified

---
+++ 01-variablesYConstantes.js
@@ -0,0 +1,14 @@
+var miVariable = 'variable creada con var';
+miVariable = 'otro valor a var'
+
+let variableLet = 'variable creada con let';
+variableLet = "otro valor a let"
+
+const constante = 'constante creada con const';
+// constante = "otro valor a const";
+// notese que esto da error porque no se puede cambiar el valor de una constante
+
+
+console.log(miVariable);
+console.log(variableLet);
+console.log(constante);
