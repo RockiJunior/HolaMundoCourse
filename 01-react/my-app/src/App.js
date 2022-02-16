f34a8d479584dfdce2bf926d1a48766c83739a08
Gabriel Villarroel <78510297+RockiJunior@users.noreply.github.com>
2022-02-16 10:11:55 -0300

    video 23 react holamundo. min 3.15

--- 01-react/my-app/src/App.js
+++ 01-react/my-app/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const estilo = (bg = '#333') => ({
+	backgroundColor: bg,
+  	color: '#fff',
+	padding: '10px 15px',
+	margin: '10px 15px'
+});
+
+const Li = ({ children }) => {
+	return (
+    // se puede cambiar el color de bg que le pasamos a la funcion estilo, pasandole
+    // el valor nuevamente('#057'). Si no se le llega a pasar un color, ya lo toma por defecto
+    // al que le pasamos en la funcion
+		<li style={estilo('#057')} className="clase-li"> 
+			{children}
+		</li>
+	);
+};
+
+const App = () => {
+	const valor = 'feliz';
+	return (
+		<ul classNem="clase-css">
+			<Li estado={valor}>Valor de Li</Li>
+		</ul>
+	);
+};
 
 export default App;
